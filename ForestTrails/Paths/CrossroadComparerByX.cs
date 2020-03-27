550823eb8e178e1f12be859fea49239ea8ddb8f9
yaroslav <gityarik@gmail.com>
2020-03-27 19:01:44 +0100

    Work on the TwoDRangeTree class.

---
+++ ForestTrails/Paths/CrossroadComparerByX.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestTrails.Paths
+{
+    class CrossroadComparerByX : IComparer<ICrossroad>
+    {
+        public int Compare(ICrossroad first, ICrossroad second)
+        {
+            return first.Position.X.CompareTo(second.Position.X);
+        }
+    }
+}
---
+++ ForestTrails/TwoDRangeTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestTrails
+{
+    public class TwoDRangeTree<TData>
+    {
+        private IComparer<TData> comparerByX;
+        private IComparer<TData> comparerByY;
+
+
+        Node RootNode { get; set; }
+
+        public TwoDRangeTree(TData[] datas, IComparer<TData> x, IComparer<TData> y)
+        {
+            comparerByX = x;
+            comparerByY = y;
+            RootNode = Build(datas);
+        }
+
+        
+        private abstract class Node
+        {
+            public bool XDimension { get; set; }
+            public Node Parent { get; set; }
+        }
+
+        private class CommonNode : Node
+        {
+            public TData Min { get; set; }
+            public TData Max { get; set; }
+            
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+            public Node OtherDimensionNode { get; set; }
+            
+        }
+
+        private class Leaf : Node
+        {
+            public TData Data { get; set; }
+            public Node LeftSibling { get; set; }
+            public Node RightSibling { get; set; }
+        }
+
+    }
+}
